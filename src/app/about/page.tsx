6d84d60c4b0a7ee5d15faacecb3cb8fa3e6c14c1
qadeer <qadeerabdul182006@gmail.com>
2025-04-04 03:29:38 +0530

    error fixes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
-import Image from 'next/image';
 import Link from 'next/link';
 
 export default function AboutPage() {
